64139b5075c37e1a61816b97028391a4825d3977
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-22 00:55:45 +0900

    chnage pre-commit and commit-template
    ## Changes
    * rename GitTemplates to git-configs
    * change templates

--- git-configs/setup.sh
+++ git-configs/setup.sh
@@ -4,7 +4,7 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR
 
 git config core.commentChar ';'
-git config commit.template GitTemplates/commit-template
+git config commit.template git-configs/commit-template
 git config core.hooksPath ./hooks
 
 chmod -R +x ./hooks
