4f3b9ef7097ae5a16a394d83870fdc5a0adcd070
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-21 22:49:12 +0900

    init project

---
+++ LayeredArchitectureSample/LayeredArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayeredArchitectureSampleApp.swift
+//  LayeredArchitectureSample
+//
+//  Created by kotaro-seki on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LayeredArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
