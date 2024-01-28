e7d35a21b91c9e750c330cf6aba5a5e8e5495149
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-28 18:24:51 +0900

    update swiftFormat version to 0.53.0

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,5 +5,5 @@ import PackageDescription
 let package = Package(name: "BuildTools",
                       platforms: [.iOS(.v17)],
                       dependencies: [
-                          .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0")
+                        .package(url: "https://github.com/nicklockwood/SwiftFormat", .upToNextMinor(from: "0.53.0"))
                       ])
