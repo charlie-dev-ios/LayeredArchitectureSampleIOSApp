697c44b4a1acad7549a44e1deddc8e122af5c49e
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-28 18:25:06 +0900

    format

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,5 +5,5 @@ import PackageDescription
 let package = Package(name: "BuildTools",
                       platforms: [.iOS(.v17)],
                       dependencies: [
-                        .package(url: "https://github.com/nicklockwood/SwiftFormat", .upToNextMinor(from: "0.53.0"))
+                          .package(url: "https://github.com/nicklockwood/SwiftFormat", .upToNextMinor(from: "0.53.0"))
                       ])
