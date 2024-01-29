af1b0796a1bf58bbd5bbaed469b09d75cb7ca5b0
charlie-dev-ios <py.yeronga@gmail.com>
2024-01-29 22:54:47 +0900

    refactor ModalController
    * the Alert can not be seen at the same time, so it would not be declared as Array
    * remove ModalDefinitions
    * the AlertTypes and SheetTypes changed to Struct instead of Enum because it is better to declare in Extension of each View or ViewModel

--- Modules/Sources/Feature/Screens/Controller/ModalController.swift
+++ Modules/Sources/Feature/Screens/Controller/ModalController.swift
@@ -11,27 +11,58 @@ import SwiftUI
 class ModalController: ObservableObject {
     static let shared = ModalController()
 
-    @Published private var modals: [ModalDefinitions] = []
+    @Published private var sheets: [SheetTypes] = []
+    @Published private var alert: AlertTypes?
 
     private init() {}
 
-    func present(_ modal: ModalDefinitions) {
-        if !modals.contains(modal) {
-            modals.append(modal)
+    func present(sheet: SheetTypes) {
+        if !sheets.contains(sheet) {
+            sheets.append(sheet)
         }
     }
 
-    func dismiss() {
-        modals.removeAll()
+    func dismissSheets() {
+        sheets.removeAll()
     }
 
-    func isPresented(_ modal: ModalDefinitions) -> Binding<Bool> {
+    func isPresented(sheet: SheetTypes) -> Binding<Bool> {
         .init(get: {
-            self.modals.contains(modal)
+            self.sheets.contains(sheet)
         }, set: { newValue in
-            if !newValue, let index = self.modals.firstIndex(of: modal) {
-                self.modals.remove(at: index)
+            if !newValue, let index = self.sheets.firstIndex(of: sheet) {
+                self.sheets.remove(at: index)
             }
         })
     }
+
+    func present(alert: AlertTypes) {
+        self.alert = alert
+    }
+
+    func isPresented(alert: AlertTypes) -> Binding<Bool> {
+        .init(get: {
+            self.alert == alert
+        }, set: { newValue in
+            if !newValue {
+                self.alert = nil
+            }
+        })
+    }
+
+    func dismissAlert() {
+        self.alert = nil
+    }
+
+    func dismissAllModals() {
+        dismissSheets()
+        dismissAlert()
+    }
+}
+struct SheetTypes: Hashable {
+    let uuid = UUID()
+}
+
+struct AlertTypes: Hashable {
+    let uuid = UUID()
 }
